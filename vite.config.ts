a0d5320d1e14f63bd9924f3f35cb04ca361e2738
me9rez <1516544795@qq.com>
2025-03-29 16:54:24 +0800

    🐎 ci: 修复路径错误问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pkg from './package.json'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: isProduction ? '/vite-bookmark-domain-analyzer/' : '/',
+  base: isProduction ? `/${pkg.name}/` : '/',
 })
