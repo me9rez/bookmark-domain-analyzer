32504a18af881cabc13b4a3cc9bd77c5ce612f41
me9rez <1516544795@qq.com>
2025-03-29 16:45:10 +0800

    🐳 chore: 更新构建配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: isProduction ? '/vite-bookmark-domain-analyzer/' : '/',
 })
